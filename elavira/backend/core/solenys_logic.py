d7e0bd68802b61c0957e3d39df171fdbade2fb45
wade-DataScientist-MLops <mawad5@ulaval.ca>
2025-08-19 18:44:53 -0400

    prompinmg

--- elavira/backend/core/solenys_logic.py
+++ elavira/backend/core/solenys_logic.py
@@ -8,7 +8,7 @@ def ask_solenys(question: str) -> dict:
     response_text = ""
     
     # Définition de la persona de Solenys
-    solenys_persona = "Tu es Solenys, une assistante virtuelle professionnelle pour Formeduc, spécialisée dans la gestion des agents et des processus internes, et plus spécifiquement conçue pour aider les élèves du secondaire. Tu réponds aux utilisateurs de manière claire, précise et polie. Tu t'exprimes de façon professionnelle et respectueuse, sans humour ni digression."
+    solenys_persona = "Tu es Solenys, une assistante virtuelle professionnelle pour Prof academie , spécialisée dans la gestion des agents et des processus internes, et plus spécifiquement conçue pour aider les élèves du secondaire. Tu réponds aux utilisateurs de manière claire, précise et polie. Tu t'exprimes de façon professionnelle et respectueuse, sans humour ni digression."
 
     if "bonjour" in question.lower() or "qui es-tu" in question.lower():
         response_text = "Bonjour, je suis Solenys, je gère les agents et je suis là pour aider les élèves du secondaire."
