bae18fa2fddc74ff9f612ab022c3102958fbd0b3
wade-DataScientist-MLops <mawad5@ulaval.ca>
2025-09-16 06:07:41 -0400

    Fix: Suppression de l'import useState non utilisé

--- formeduc-react/src/components/Dashboard/AgentsManagementDashboard.tsx
+++ formeduc-react/src/components/Dashboard/AgentsManagementDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useApp } from '../../context/AppContext';
 import { AgentType } from '../../types';
